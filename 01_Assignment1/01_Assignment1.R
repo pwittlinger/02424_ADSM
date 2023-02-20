ddec14e710b031c291f2fd8e02aea75d8f2746f6
unknown <paul.wittlinger@web.de>
2023-02-20 16:18:38 +0100

    install ggplot

--- 01_Assignment1/01_Assignment1.R
+++ 01_Assignment1/01_Assignment1.R
@@ -1,13 +1,11 @@
+library(dplyr)
+#install.packages("ggplot2")
+library(ggplot2)
 ### Assignment1
-
+# Reading in the data
 dioxin<-read.csv("../01_Assignment1/dioxin.csv",header=T)
 attach(dioxin)
 names(dioxin)
 
-pairs(dioxin,panel=panel.smooth)
-
-
-cov(dioxin)
-cor(dioxin)
-library(corrplot)
-corrplot(cor(dioxin))
+# Transforming the variables into factors
+dioxin <- mutate_if(dioxin, is.character, as.factor)
