562c25811da7a386d867402dbc0718f29b8fac8a
unknown <paul.wittlinger@web.de>
2023-02-20 16:46:46 +0100

    some fancy plots

--- 01_Assignment1/01_Assignment1.R
+++ 01_Assignment1/01_Assignment1.R
@@ -1,11 +1,21 @@
 library(dplyr)
 #install.packages("ggplot2")
 library(ggplot2)
+#install.packages("GGally")
+#library(GGally)
 ### Assignment1
 # Reading in the data
 dioxin<-read.csv("../01_Assignment1/dioxin.csv",header=T)
-attach(dioxin)
 names(dioxin)
 
 # Transforming the variables into factors
 dioxin <- mutate_if(dioxin, is.character, as.factor)
+
+
+df <- dioxin  %>% dplyr::select(where(is.numeric))
+pairs(df)
+
+
+p <- ggplot(dioxin, aes(x=PLANT, y=DIOX, fill=LAB)) + geom_boxplot(outlier.size = 2, notch=FALSE, outlier.shape = 16, outlier.colour = "black")
+p
+
