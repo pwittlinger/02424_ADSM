f694622f5f1821a2e13a4dc2adb2214886a239f3
unknown <paul.wittlinger@web.de>
2023-03-25 17:35:04 +0100

    added code

--- 02_Assignment2/01_Assignment2.R
+++ 02_Assignment2/01_Assignment2.R
@@ -41,6 +41,10 @@ df$sex <- data$sex
 pairs(df)
 GGally::ggpairs(df)
 
+png(filename="pairplot.png")
+dplyr::select(df,c("clo", "tOut", "tInOp", "sex")) %>% GGally::ggpairs()
+dev.off()
+
 
 ##############
 ## Exercise 1
@@ -49,22 +53,67 @@ GGally::ggpairs(df)
 
 df1 <- dplyr::select(data,c("clo", "tOut", "tInOp", "sex"))
 
+# first we fit the simplest model without any interactions
+lm.no <- lm(clo~tOut+tInOp+sex, data=df1)
+summary(lm.no)
+par(mfrow=c(2,2))
+plot(lm.no)
+
+lm.s2 <- lm(clo~(tOut+tInOp+sex)^2, data=df1)
+summary(lm.s2)
+anova(lm.no, lm.s2)
+plot(lm.s2)
+
+
 lm.1 <- lm(clo~(tOut+tInOp+sex)^3, data=df1)
 summary(lm.1)
+
+
+par(mfrow=c(2,2))
 plot(lm.1)
 logLik(lm.1)
+anova(lm.s2, lm.1)
+summary(lm.1)
 
+# Even the fully specified model is quite poor at explaining the variation in the data
 
 lm.2 <- lm(clo~poly(tOut,2)*sex*poly(tInOp,2), data=df1)
 summary(lm.2)
+AIC(lm.2)
+anova(lm.1, lm.2)
+logLik(lm.2)
+
+# Backwards selection leads to the same model, meaning it already is the best model to choose
+#lm.2small <- step(lm.2)
+#summary(lm.2small)
 
 par(mfrow=c(2,2))
 plot(lm.2)
 dev.off()
 
+####
+#
+
+glm.no <- glm(clo~(tOut+tInOp+sex), family = Gamma, data = df1)
+summary(glm.no)
+plot(glm.no)
+
+glm.0 <- glm(clo~(tOut+tInOp+sex)^2, family = Gamma(link = "inverse"), data = df1)
+summary(glm.0)
+#glm(clo~(tOut+tInOp+sex)^2, family = Gamma, data = df1) %>% summary()
+plot(glm.0)
+anova(glm.no, glm.0, test="F")
+anova(glm.0, test="F")
+AIC(glm.0)
+
+
 glm.1 <- glm(clo~(tOut+tInOp+sex)^3, family = Gamma, data = df1)
 summary(glm.1)
+plot(glm.1)
 logLik(glm.1)
+
+anova(glm.0, glm.1, test="F")
+# Increasing the model order any further does not yield any significant improvement in explanatory power
 
 glm.2 <- glm(clo~poly(tOut,2)*sex*poly(tInOp,2),family=Gamma, data=df1)
 summary(glm.2)
@@ -72,14 +121,63 @@ logLik(glm.2)
 par(mfrow=c(2,2))
 plot(glm.2)
 
+anova(glm.0, glm.2, test="F")
+
 
 ##############
 #
 
+glm.0$residuals %>% dplyr::select
+
+df$sex %>% dplyr::select(0)
+
+resdat <- cbind(glm.0$residuals, df$sex)
+colnames(resdat) <- c("residuals", "sex")
+resdat <- data.frame(resdat)
+resdat$sex <- as.factor(resdat$sex)
+resdat$sex <- df$sex
+
+p <- ggplot(resdat, aes(x=sex, y=residuals)) + geom_boxplot(outlier.size = 2, notch=FALSE, outlier.shape = 16, outlier.colour = "black")
+p
+
+var_m <- resdat %>% dplyr::filter(sex=="male") %>% dplyr::select("residuals")# %>% var
+
+var_f <- resdat %>% dplyr::filter(sex=="female") %>% dplyr::select("residuals")# %>% var
 
 
+var.test(unlist(var_m), unlist(var_f))
+
+######
+# Fit the model now with subjectId instead of 
+
+df2 <- dplyr::select(data,c("clo", "tOut", "tInOp", "subjId"))
+
+glm.2subj <- glm(clo~poly(tOut,2)*subjId*poly(tInOp,2),family=Gamma, data=df2)
+
+summary(glm.2subj)
+anova(glm.2subj, test="F")
+plot(glm.2subj)
+
+glm.0subj <-glm(clo~(tOut)*subjId*(tInOp),family=Gamma, data=df2)
+summary(glm.0subj)
+plot(glm.0subj)
+
+anova(glm.0subj,glm.2subj, test="F")
+
+glm.2subj$residuals
+
+df_res <- df2
+df_res$residuals <- glm.2subj$residuals
+df_res$day <- data$day
+df_res$time <- data$time
 
 
+df_res %>% dplyr::group_by(subjId,day) %>% dplyr::arrange(subjId, day, time)
+dev.off()
+
+df_res$residuals[0:6]
+
+acf(df_res$residuals[0:6],6)
 
 # Looking at the raw dependent variable
 ## we can see that there are quite some outliers
